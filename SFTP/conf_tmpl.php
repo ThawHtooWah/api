3a776d102e54ff9a1597296d3c2207a6f0a5eb98
Thaw Htoo Wah <thawhtoowah25@gmail.com>
2022-07-05 16:47:24 +0630

    add config template for SFTP

---
+++ SFTP/conf_tmpl.php
@@ -0,0 +1,10 @@
+<?php
+   // Configurations for SFTP
+   define('SFTP_HOST', '');
+   define('SFTP_PORT', '');
+   define('SFTP_USER', '');
+   define('SFTP_PWD', '');
+   define('SFTP_PRIVATE_KEY', '');
+   define('SFTP_REMOTE_DIR', '');
+   define('SFTP_LOCAL_DIR', '');
+?>
